5ed193e2df6898c6956347bd2419b4efa379dcf7
Matheus Enrike Ramalho Ortiz <24004134870@info.local>
2023-08-05 21:15:04 -0300

    Somente o necessario

--- sprint1-bd/exercicios/1.4-exercicio-optus/Exercicio_1.4_DML.sql
+++ sprint1-bd/exercicios/1.4-exercicio-optus/Exercicio_1.4_DML.sql
@@ -7,10 +7,10 @@ insert into Album
 values(2, 'ImagineDragons', '1:00:00', '00/00/0000', 'Rock', 'AlbumImagineDragons', 'Lugar3', 1)
 
 insert into Musica
-values(1, 'Master of Puppets'), (2, 'Um Bom Lugar'), (3, 'Thunder')
+values(2, 'Master of Puppets'), (2, 'Um Bom Lugar'), (2, 'Thunder')
 
 insert into Estilo
-values(2, 'Rap')
+values(3, 'Rap')
 
 insert into Usuario
 values('Matheus', 'matheus@email.com', '1234', 1)
--- sprint1-bd/exercicios/1.4-exercicio-optus/Exercicio_1.4_DQL.sql
+++ sprint1-bd/exercicios/1.4-exercicio-optus/Exercicio_1.4_DQL.sql
@@ -2,6 +2,22 @@
 
 USE Exercicio_1_4_Tarde;
 
+--- listar todos os usuários administradores, sem exibir suas senhas
+select Usuario.Nome as Nome_do_Usuario from Usuario
+where Usuario.NivelPermissao = 1
+
+--- listar todos os álbuns lançados após o um determinado ano de lançamento
+select * from Album
+where Album.Lancamento like '%2000'
+
+--- listar os dados de um usuário através do e-mail e senha
+select * from Usuario
+where Usuario.Email like 'matheus@email.com' and Usuario.Senha like '1234'
+
+--- listar todos os álbuns ativos, mostrando o nome do artista e os estilos do álbum 
+select * from Album
+where Album.ATIVO = 1
+
 SELECT*FROM Artista
 SELECT*FROM Album
 SELECT*FROM Musica

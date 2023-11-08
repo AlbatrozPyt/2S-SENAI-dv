c95f227c109089e2e0b7c1c7ea522738bba5a769
Matheus Enrike Ramalho Ortiz <matheus@senai>
2023-11-08 17:19:01 -0300

    eventplus

--- React/eventplus/src/pages/HomePage/HomePage.jsx
+++ React/eventplus/src/pages/HomePage/HomePage.jsx
@@ -1,26 +1,35 @@
 import React from "react";
+import "./HomePage.css";
 import Title from "../../Components/Titulo/Title";
 import MainContent from "../../Components/MainContent/MainContent";
 import Banner from "../../Components/Banner/Banner";
 import VisionSection from "../../Components/VisionSection/VisionSection";
 import ContactSection from "../../Components/ContactSection/ContactSection";
+import Container from "../../Components/Container/Container";
 import NextEvent from "../../Components/NextEvent/NextEvent";
 
 const HomePage = () => {
   return (
     <MainContent>
-      <Banner/>
+      <Banner />
 
-      <NextEvent
-        title={"C# na pratica"}
-        description={"Evento pratico para a linguagem de programação C#."}
-        eventDate={"12/12/2023"}
-      />
+      <section className="proximos-eventos">
+        <Container>
+          <Title titleText={"Proximos Eventos"}/>
 
-      <VisionSection/>
-      <ContactSection/>
+          <div className="event-box">
+            <NextEvent
+              title={"C# na pratica"}
+              description={"Evento da linguagem de programação C#."}
+              eventDate={"12/12/2023"}
+            />
+          </div>
 
-   
+        </Container>
+      </section>
+
+      <VisionSection />
+      <ContactSection />
     </MainContent>
   );
 };

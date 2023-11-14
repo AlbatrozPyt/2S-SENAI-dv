dbcf1fc4e0adf508795ab944d03caccb539805df
Matheus Enrike Ramalho Ortiz <matheus@senai>
2023-11-14 17:10:13 -0300

    eventplus

--- React/eventplus/src/components/FormComponents/FormComponents.jsx
+++ React/eventplus/src/components/FormComponents/FormComponents.jsx
@@ -1,27 +1,41 @@
-import React from 'react';
-import "./FormComponents.css"
+import React from "react";
+import "./FormComponents.css";
 
-export const Input = ( {
-    type,
-    id,
-    value,
-    required,
-    additionalClass,
-    name,
-    placeholder,
-    manipulationFunction
-} ) => {
-    return (
-        <input 
-            type={type}
-            id={id}
-            value={value}
-            name={name}
-            required={required}
-            className={`input-component ${additionalClass}`}
-            placeholder={placeholder}
-            onChange={manipulationFunction}
-            autoComplete="off"
-        />
-    )
-}
+export const Input = ({
+  type,
+  id,
+  value,
+  required,
+  additionalClass,
+  name,
+  placeholder,
+  manipulationFunction,
+}) => {
+  return (
+    <input
+      type={type}
+      id={id}
+      value={value}
+      name={name}
+      required={required}
+      className={`input-component ${additionalClass}`}
+      placeholder={placeholder}
+      onChange={manipulationFunction}
+      autoComplete="off"
+    />
+  );
+};
+
+export const Button = ({ id, name, type, additionalClass="", manipulationFunction, textButton }) => {
+  return (
+    <button
+      type={type}
+      name={name}
+      id={id}
+      className={`button-component ${additionalClass}`}
+      onClick={manipulationFunction}
+    >
+      {textButton}
+    </button>
+  );
+};
--- React/eventplus/src/pages/TipoEventos/TipoEventos.jsx
+++ React/eventplus/src/pages/TipoEventos/TipoEventos.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TipoEventos.css";
 import Title from "../../Components/Titulo/Title";
 import MainContent from "../../Components/MainContent/MainContent";
 import ImageIllustrator from "../../Components/ImageIlustrator/ImageIllustrator";
 import Container from "../../Components/Container/Container";
 import eventTypeImage from "../../assets/images/tipo-evento.svg";
-import { Input } from "../../Components/FormComponents/FormComponents";
+import api from "../../Services/Services"
+import { Input, Button } from "../../Components/FormComponents/FormComponents";
 
 const TipoEventos = () => {
+  const [frmEdit, setFrmEdit] = useState(false);
+  const [titulo, setTitulo] = useState("");
+
+  async function handleSubmit(e) {
+    // parar submit do form
+    e.preventDefault();
+
+    // validar pelo menos 3 caracteres
+    if(titulo.trim().length < 3) {
+      alert("O titulo deve ter pelo menos 3 caracteres");
+      return;
+    }
+
+    // chamar api
+    try {
+      const retorno = await api.post("/TiposEvento", {titulo: titulo})
+      console.log("Cadastrado com sucesso!!!");
+      console.log(retorno.data);
+      setTitulo(""); // limpa a variavel
+    } catch (error) {
+      console.log("Deu ruim na api");
+      console.log(error);
+    }
+  }
+
+  function handleUpdate() {
+    alert("Atualizar");
+  }
+
   return (
     <MainContent>
       <section className="cadastro-evento-section">
@@ -19,12 +49,30 @@ const TipoEventos = () => {
               imageRender={eventTypeImage}
             />
 
-            <form onSubmit={frmEdit ? handleUpdate : handleSubmit}>
-              <p>Componente de Formulario</p>
-              <Input 
-                type={"number"}
-                required={"required"}
-              />
+            <form className="ftipo-evento" onSubmit={frmEdit ? handleUpdate : handleSubmit}>
+              {!frmEdit ? (
+                // Cadastrar
+                <>
+                  <Input
+                    type={"text"}
+                    id={"titulo"}
+                    name={"titulo"}
+                    placeholder={"Titulo"}
+                    required={"required"}
+                    value={titulo}
+                    manipulationFunction={
+                      (e) => {
+                        setTitulo(e.target.value)
+                      }
+                    }
+                  />
+                  <span>{titulo}</span>
+
+                  <Button type={"submit"} name={"cadastar"} id={"cadastar"} textButton={"Cadastar"}/>
+                </>
+              ) : (
+                <p>Tela de edição</p>
+              )}
             </form>
           </div>
         </Container>

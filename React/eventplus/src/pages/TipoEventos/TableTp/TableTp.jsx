7e401399d5bfb0608ce1873368ac838ee6e6a17f
Matheus Enrike Ramalho Ortiz <matheus@senai>
2023-11-16 17:20:45 -0300

    eventplus

---
+++ React/eventplus/src/pages/TipoEventos/TableTp/TableTp.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./TableTp.css";
+
+import editPen from "../../../assets/images/edit-pen.svg";
+import trashDelete from "../../../assets/images/trash-delete.svg";
+
+const TableTp = ({ dados, fnUpdate, fnDelete }) => {
+  console.log(dados);
+  return (
+    <table className="table-data">
+      <thead className="table-data__head">
+        <tr className="table-data__head-row">
+          <th className="table-data__head-title table-data__head-title--big">
+            Título
+          </th>
+          <th className="table-data__head-title table-data__head-title--little">
+            Editar
+          </th>
+          <th className="table-data__head-title table-data__head-title--little">
+            Deletar
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {dados.map((e) => {
+          return (
+            <tr className="table-data__head-row">
+              <td className="table-data__data table-data__data--big">
+                {e.titulo}
+              </td>
+
+              <td className="table-data__data table-data__data--little">
+                <img
+                  className="table-data__icon"
+                  src={editPen}
+                  onClick={() => {
+                    fnUpdate();
+                  }}
+                />
+              </td>
+
+              <td className="table-data__data table-data__data--little">
+                <img
+                  className="table-data__icon"
+                  src={trashDelete}
+                  onClick={() => {
+                    fnDelete();
+                  }}
+                />
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default TableTp;
--- React/eventplus/src/pages/TipoEventos/TipoEventos.jsx
+++ React/eventplus/src/pages/TipoEventos/TipoEventos.jsx
@@ -4,13 +4,21 @@ import Title from "../../Components/Titulo/Title";
 import MainContent from "../../Components/MainContent/MainContent";
 import ImageIllustrator from "../../Components/ImageIlustrator/ImageIllustrator";
 import Container from "../../Components/Container/Container";
+import TableTps from "./TableTp/TableTp";
 import eventTypeImage from "../../assets/images/tipo-evento.svg";
 import api from "../../Services/Services"
 import { Input, Button } from "../../Components/FormComponents/FormComponents";
+import TableTp from "./TableTp/TableTp";
 
 const TipoEventos = () => {
   const [frmEdit, setFrmEdit] = useState(false);
   const [titulo, setTitulo] = useState("");
+  
+  const [tipoEventos, setTipoEventos] = useState([
+    {idTipoEvento: "123", titulo: "Evento ABC"},
+    {idTipoEvento: "555", titulo: "Evento xpto"},
+    {idTipoEvento: "444", titulo: "Evento de JavaScript"}
+  ]); // arrsy mocado
 
   async function handleSubmit(e) {
     // parar submit do form
@@ -34,12 +42,27 @@ const TipoEventos = () => {
     }
   }
 
+  //* EDITAR CADASTRO
+
   function handleUpdate() {
     alert("Atualizar");
   }
 
+  function showUpdateForm() {
+    alert("Mostrando a tela de update")
+  }
+
+  function handleDelete() {
+    alert("Bora lá apagar na api")
+  }
+
+  function editActionAbort() {
+    alert("Cancelar a tela de edição de dados")
+  }
+
   return (
     <MainContent>
+      {/* Cadastro de tipos de eventos */}
       <section className="cadastro-evento-section">
         <Container>
           <div className="cadastro-evento__box">
@@ -66,7 +89,7 @@ const TipoEventos = () => {
                       }
                     }
                   />
-                  <span>{titulo}</span>
+                  {/* <span>{titulo}</span> */}
 
                   <Button type={"submit"} name={"cadastar"} id={"cadastar"} textButton={"Cadastar"}/>
                 </>
@@ -75,6 +98,19 @@ const TipoEventos = () => {
               )}
             </form>
           </div>
+        </Container>
+      </section>
+
+      {/* Listagem de tipos de eventos */}
+      <section className="lista-eventos-section">
+        <Container>
+          <Title titleText={"Lista Tipo de Eventos"} color="white"/>
+
+          <TableTp
+            dados={tipoEventos}
+            fnUpdate={showUpdateForm}
+            fnDelete={handleDelete}
+          />
         </Container>
       </section>
     </MainContent>
